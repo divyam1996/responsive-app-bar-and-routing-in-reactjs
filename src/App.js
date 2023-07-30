221306b000201eedb9e15615fc3c0581f1b61272
Divyam Bansal <divyambansal17@gmail.com>
2023-07-30 23:09:24 +0530

    app.js changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, Link, Outlet, useRoutes } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Navbar/>
     </div>
+    </BrowserRouter>
   );
 }
 
