eca3b40a3d4b08595f2bc26656c06e8869bc38aa
Divyam Bansal <divyambansal17@gmail.com>
2023-07-31 13:42:39 +0530

    pages added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter, Link, Outlet, useRoutes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about';
+import Events from './pages/events';
+import AnnualReport from './pages/annual';
+import Teams from './pages/team';
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
 
 function App() {
-  return (
-    <BrowserRouter>
-    <div className="App">
-      <Navbar/>
-    </div>
-    </BrowserRouter>
-  );
+return (
+	<Router>
+	<Navbar />
+	<Routes>
+		<Route path='/' exact element={<Home/>} />
+		<Route path='/about' element={<About/>} />
+		<Route path='/events' element={<Events/>} />
+		<Route path='/annual' element={<AnnualReport/>} />
+		<Route path='/team' element={<Teams/>} />
+		<Route path='/blogs' element={<Blogs/>} />
+		<Route path='/sign-up' element={<SignUp/>} />
+	</Routes>
+	</Router>
+);
 }
 
 export default App;
